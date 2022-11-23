9a56a0dc00be2696cbdd0f666c26beb4066e8b74
tchang135 <tchang135@gmail.com>
2022-11-23 14:34:35 -0500

    working on deep dup

--- recursion_exercises.rb
+++ recursion_exercises.rb
@@ -3,17 +3,17 @@ def range(min, max)
     range(min, max-1) + [max-1]  
 end
 
-p "range"
-p range(1,5)
-p range(2,15)
+# p "range"
+# p range(1,5)
+# p range(2,15)
 
 def range_itr(min, max)
     (min...max).map {|ele| ele}
 end
 
-p "range_itr"
-p range_itr(1,5)
-p range_itr(2,15)
+# p "range_itr"
+# p range_itr(1,5)
+# p range_itr(2,15)
 
 def exponentiation1(base, power)
     return base if power == 1
@@ -22,6 +22,22 @@ def exponentiation1(base, power)
     base * exponentiation1(base, power-1)
 end
 
-p "exponentiation1"
-p exponentiation1(2, 3)
-p exponentiation1(2, 7)
+# p "exponentiation1"
+# p exponentiation1(2, 3)
+# p exponentiation1(2, 7)
+
+
+def exponentiation2(base, power)
+    return base if power == 1 
+    return (exponentiation2(base, power/2) ** 2) * base if power.even?
+    return (exponentiation2(base, (power-1)/2) ** 2) * base if power.odd?
+end 
+
+# p "exponentiation2"
+# p exponentiation2(2, 3)
+# p exponentiation2(2, 7)
+
+
+def deep_dup(arr)
+    arr = arr.dup 
+end 
